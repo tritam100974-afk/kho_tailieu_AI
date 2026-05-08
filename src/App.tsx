7e0248b85ec060ab1245c185c327937bab61c9b1
tritam100974-afk <tritam100974@gmail.com>
2026-05-08 15:46:49 +0700

    refactor: Update instructor name translation
    Translate "Giáo viên hướng dẫn" to "Instructor" in the footer for better clarity in English.

--- src/App.tsx
+++ src/App.tsx
@@ -239,7 +239,7 @@ export default function App() {
           </div>
           <div className="font-black text-xs opacity-40 tracking-wider text-right leading-relaxed">
             Made by Nguyễn Gia Bảo - Lớp 7.2<br/>
-            Giáo viên hướng dẫn: Nguyễn Tri Thị Tâm
+            Instructor: Nguyễn Tri Thị Tâm
           </div>
         </div>
       </footer>
